c94adbc4c34ebe9951b745f16dcc4ef8827c6039
Michael Russo <hpe_mrusso@homesite.com>
2020-11-15 16:59:59 -0500

    Updates docker tag to use Commit Hash
    - Moves parameter values to deploy-infra.sh

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -4,17 +4,13 @@ STACK_NAME=aws-batch-quickstart
 REGION=us-east-1
 CLI_PROFILE=aws-homesite-home-sandbox-acct
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity \
-        --query "Account" --output text --profile $CLI_PROFILE`
-
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity \
+  --query "Account" --output text --profile $CLI_PROFILE)
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
-
 CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
-
-# EC2_INSTANCE_TYPE=t2.micro
-# DOMAIN=the-good-parts.com
-# CERT=`aws acm list-certificates --region $REGION --profile awsbootstrap --output text \
-#         --query "CertificateSummaryList[?DomainName=='$DOMAIN'].CertificateArn | [0]"`
+SUBNETS="subnet-02e4835df727d512d,subnet-0830c53ec19d9e77d"
+SECURITY_GROUP_IDS="sg-028f8e6feb32d8433"
+IMAGE_REPO_NAME="batch-processing-job-repository"
 
 # Generate a personal access token with repo and admin:repo_hook
 #    permissions from https://github.com/settings/tokens
@@ -33,8 +29,8 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET \
-    CloudFormationBucket=$CFN_BUCKET
+  CodePipelineBucket=$CODEPIPELINE_BUCKET \
+  CloudFormationBucket=$CFN_BUCKET
 
 # Package up CloudFormation templates into an S3 bucket
 echo -e "\n\n=========== Packaging main.yml ==========="
@@ -63,11 +59,14 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    GitHubOwner=$GH_OWNER \
-    GitHubRepo=$GH_REPO \
-    GitHubBranch=$GH_BRANCH \
-    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
-    CodePipelineBucket=$CODEPIPELINE_BUCKET
+  GitHubOwner=$GH_OWNER \
+  GitHubRepo=$GH_REPO \
+  GitHubBranch=$GH_BRANCH \
+  GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET \
+  Subnets=$SUBNETS \
+  SecurityGroupIds=$SECURITY_GROUP_IDS \
+  RepositoryName=$IMAGE_REPO_NAME
 
 # If the deploy succeeded, list exports
 if [ $? -eq 0 ]; then
